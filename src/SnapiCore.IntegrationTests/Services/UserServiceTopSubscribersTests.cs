51d02bfb444867f99ce1d919518b62f5910c81e6
Evsey Zholobov <evsei124@gmail.com>
2021-06-05 02:18:56 +0500

    more tests

---
+++ src/SnapiCore.IntegrationTests/Services/UserServiceTopSubscribersTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using SnapiCore.Data;
+using SnapiCore.Services;
+using Xunit;
+
+namespace SnapiCore.IntegrationTests.Services
+{
+    public class UserServiceTopSubscribersTests
+    {
+        public async Task Fill(UsersService service, string top)
+        {
+            await service.CreateUserAsync(top);
+            var count = service.GetTopPersons(1).FirstOrDefault()?.SubscribersCount ?? 0;
+            foreach (var _ in Enumerable.Range(0, count + 1))
+            {
+                var user = Guid.NewGuid().ToString();
+                await service.CreateUserAsync(user);
+
+                await service.SubscribeAsync(user, top);
+            }
+        }
+        [Fact]
+        public async Task TopSubscribers_UserWithMaximumSubscribers_UserInTop()
+        {
+            var topUser = Guid.NewGuid().ToString();
+            await using var context = DbContextFactory.Get();
+            var service = new UsersService(context);
+            await Fill(service, topUser);
+
+            var result = service.GetTopPersons(2);
+
+            var userDto = result.FirstOrDefault();
+            Assert.NotNull(userDto);
+            Assert.Equal(topUser, userDto.Name);
+            Assert.NotEqual(userDto.Name, result.Skip(1).FirstOrDefault()?.Name);
+        }
+    }
+}
